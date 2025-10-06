1042a60b97ecf62d503d5213b39162c24d640b45
zachshoaib <zacharia.shoaib@aa.com>
2025-10-06 14:52:42 -0500

    form validation

--- src/components/CreateRoom.jsx
+++ src/components/CreateRoom.jsx
@@ -15,19 +15,28 @@ const CreateRoom = ({ onClose }) => {
     <div>
       <div className={styles.modal}>
         <div className={styles.modalContent}>
-          <h2>Set Room Name</h2>
-          <input type="text" value={roomId} onChange={(e) => setRoomId(e.target.value)} placeholder="Enter Room ID" />
-          <h2>Set Player Name</h2>
-          <input
-            type="text"
-            value={playerName}
-            onChange={(e) => setPlayerName(e.target.value)}
-            placeholder="Enter Player Name"
-          />
-          <div className={styles.modalActions}>
-            <button onClick={handleCreate}>Create Room</button>
-            <button onClick={onClose}>Cancel</button>
-          </div>
+          <form onSubmit={handleCreate}>
+            <h2>Set Room Name</h2>
+            <input
+              type="text"
+              required
+              value={roomId}
+              onChange={(e) => setRoomId(e.target.value)}
+              placeholder="Enter Room ID"
+            />
+            <h2>Set Player Name</h2>
+            <input
+              type="text"
+              required
+              value={playerName}
+              onChange={(e) => setPlayerName(e.target.value)}
+              placeholder="Enter Player Name"
+            />
+            <div className={styles.modalActions}>
+              <button type="submit">Create Room</button>
+              <button onClick={onClose}>Cancel</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
