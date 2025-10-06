2387d40852e4b57387d5dd66d195ca2278d2f2d8
zachshoaib <zacharia.shoaib@aa.com>
2025-10-06 16:54:03 -0500

    working lobby socket list

--- src/components/LobbyRoom.jsx
+++ src/components/LobbyRoom.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import styles from "../css/LobbyRoom.module.css";
+import { socket } from "../socket";
 
 const LobbyRoom = () => {
-  const { roomId: serverId } = useParams();
-  
+  const { roomId: roomId } = useParams();
+  const [players, setPlayers] = useState([]);
+
+  socket.emit("getPlayers", roomId);
+
+  useEffect(() => {
+    socket.on("playersList", (players) => {
+      setPlayers(players);
+    });
+  }, []);
+
   return (
     <div>
-      <h2>Lobby Room: {serverId}</h2>
+      <h2>Lobby Room: {roomId}</h2>
       <p>Welcome to the lobby! Waiting for players to join...</p>
+
+      <div className={styles.playerList}>
+        <h4>Players in lobby:</h4>
+        <ul>
+          {players.map((player, index) => (
+            <li key={index}>{player}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
