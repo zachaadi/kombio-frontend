ac32fa7d6970a815a889b9c893da216b2d2adbc6
zachshoaib <zacharia.shoaib@aa.com>
2025-10-08 21:44:36 -0500

    poor grid for lobby room

--- src/components/LobbyRoom.jsx
+++ src/components/LobbyRoom.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styles from "../css/LobbyRoom.module.css";
 import { socket } from "../socket";
+import { Container, Box, Grid } from "@mui/material";
 
 const LobbyRoom = () => {
   const { roomId: roomId } = useParams();
@@ -16,19 +17,25 @@ const LobbyRoom = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Lobby Room: {roomId}</h2>
-      <p>Welcome to the lobby! Waiting for players to join...</p>
-
-      <div className={styles.playerList}>
-        <h4>Players in lobby:</h4>
-        <ul>
-          {players.map((player, index) => (
-            <li key={index}>{player}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <Container>
+      <Grid container spacing={10}>
+        <Grid item size={3} className={styles.playerListBox}>
+          <h4>Players in lobby</h4>
+          <ul>
+            {players.map((player, index) => (
+              <li key={index}>{player}</li>
+            ))}
+          </ul>
+        </Grid>
+        <Grid item size={6} className={styles.gameConfigureBox}>
+          <h2>Lobby Name: {roomId}</h2>
+          <p>Welcome to the lobby! Waiting for players to join...</p>
+        </Grid>
+        <Grid item size={3} className={styles.chatBox}>
+          <h4>Chat box</h4>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
