4baee8329484112a8aaa0f7fd5c7f7d5c8aa322d
zachshoaib <zacharia.shoaib@aa.com>
2025-10-11 12:29:15 -0500

    removing formatting

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import Header from "./components/Header.jsx";
+import { Toolbar } from "@mui/material";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Header />
-      <div style={{ paddingTop: "2em" }}></div>
+      <Toolbar />
       <App />
     </BrowserRouter>
   </StrictMode>
