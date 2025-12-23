47606bc665ed6b9437c65e05a976f326de0ee1f4
zachshoaib <zachshoaib@pm.me>
2025-12-23 13:02:07 -0600

    feat: create deck and discard objects

--- src/components/game-room/GameBoard.tsx
+++ src/components/game-room/GameBoard.tsx
@@ -1,10 +1,12 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Tooltip } from "@mui/material";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { socket } from "../../app/socket";
 import { setGame, Game } from "./GameSlice";
 import PlayerHand from "../player/PlayerHand";
+import deck from "/deck.svg";
+import backCard from "/back-card.svg";
 
 const GameBoard = () => {
   const dispatch = useDispatch();
@@ -59,6 +61,21 @@ const GameBoard = () => {
             key={players[(playerIndex + 1) % players.length].name}
             name={players[(playerIndex + 1) % players.length].name || ""}
           />
+
+          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 4 }}>
+            <Box sx={{ pt: "2em", pb: "2em" }}>
+              <Box>Deck</Box>
+              <Tooltip title={"Draw card"}>
+                <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={deck} />
+              </Tooltip>
+            </Box>
+
+            <Box sx={{ pt: "2em", pb: "2em" }}>
+              <Box>Discard</Box>
+              <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={backCard} />
+            </Box>
+          </Box>
+
           <PlayerHand key={players[playerIndex].name} name={players[playerIndex].name || ""} />
         </Box>
       );
@@ -67,7 +84,7 @@ const GameBoard = () => {
     if (numPlayers === 3) {
       return (
         <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <Box sx={{ display: "flex", gap: 4, justifyContent: "center" }}>
+          <Box sx={{ display: "flex", gap: 14, justifyContent: "center" }}>
             <PlayerHand
               key={players[(playerIndex + 1) % players.length].name}
               name={players[(playerIndex + 1) % players.length].name || ""}
@@ -77,6 +94,21 @@ const GameBoard = () => {
               name={players[(playerIndex + 2) % players.length].name || ""}
             />
           </Box>
+
+          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 4 }}>
+            <Box sx={{ pt: "2em", pb: "2em" }}>
+              <Box>Deck</Box>
+              <Tooltip title={"Draw card"}>
+                <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={deck} />
+              </Tooltip>
+            </Box>
+
+            <Box sx={{ pt: "2em", pb: "2em" }}>
+              <Box>Discard</Box>
+              <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={backCard} />
+            </Box>
+          </Box>
+
           <Box sx={{ display: "flex", justifyContent: "center" }}>
             <PlayerHand key={players[playerIndex].name} name={players[playerIndex].name || ""} />
           </Box>
@@ -93,11 +125,26 @@ const GameBoard = () => {
               name={players[(playerIndex + 3) % players.length].name || ""}
             />
           </Box>
-          <Box sx={{ display: "flex", gap: 4, justifyContent: "center" }}>
+          <Box sx={{ display: "flex", gap: 14, justifyContent: "center" }}>
             <PlayerHand
               key={players[(playerIndex + 1) % players.length].name}
               name={players[(playerIndex + 1) % players.length].name || ""}
             />
+
+            <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 4 }}>
+              <Box sx={{ pt: "2em", pb: "2em" }}>
+                <Box>Deck</Box>
+                <Tooltip title={"Draw card"}>
+                  <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={deck} />
+                </Tooltip>
+              </Box>
+
+              <Box sx={{ pt: "2em", pb: "2em" }}>
+                <Box>Discard</Box>
+                <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={backCard} />
+              </Box>
+            </Box>
+
             <PlayerHand
               key={players[(playerIndex + 2) % players.length].name}
               name={players[(playerIndex + 2) % players.length].name || ""}
@@ -113,7 +160,7 @@ const GameBoard = () => {
     if (numPlayers === 5) {
       return (
         <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <Box sx={{ display: "flex", gap: 4, justifyContent: "center" }}>
+          <Box sx={{ display: "flex", gap: 14, justifyContent: "center" }}>
             <PlayerHand
               key={players[(playerIndex + 3) % players.length].name}
               name={players[(playerIndex + 3) % players.length].name || ""}
@@ -123,11 +170,26 @@ const GameBoard = () => {
               name={players[(playerIndex + 4) % players.length].name || ""}
             />
           </Box>
-          <Box sx={{ display: "flex", gap: 4, justifyContent: "center" }}>
+          <Box sx={{ display: "flex", gap: 14, justifyContent: "center" }}>
             <PlayerHand
               key={players[(playerIndex + 1) % players.length].name}
               name={players[(playerIndex + 1) % players.length].name || ""}
             />
+
+            <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 4 }}>
+              <Box sx={{ pt: "2em", pb: "2em" }}>
+                <Box>Deck</Box>
+                <Tooltip title={"Draw card"}>
+                  <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={deck} />
+                </Tooltip>
+              </Box>
+
+              <Box sx={{ pt: "2em", pb: "2em" }}>
+                <Box>Discard</Box>
+                <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={backCard} />
+              </Box>
+            </Box>
+
             <PlayerHand
               key={players[(playerIndex + 2) % players.length].name}
               name={players[(playerIndex + 2) % players.length].name || ""}
@@ -148,21 +210,39 @@ const GameBoard = () => {
               name={players[(playerIndex + 5) % players.length].name || ""}
             />
           </Box>
-          <Box sx={{ display: "flex", gap: 4, justifyContent: "center" }}>
+          <Box sx={{ display: "flex", gap: 14, justifyContent: "center" }}>
             <PlayerHand
               key={players[(playerIndex + 3) % players.length].name}
               name={players[(playerIndex + 3) % players.length].name || ""}
             />
+
+            <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 4 }}>
+              <Box sx={{ pt: "2em", pb: "2em" }}>
+                <Box>Deck</Box>
+                <Tooltip title={"Draw card"}>
+                  <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={deck} />
+                </Tooltip>
+              </Box>
+            </Box>
+
             <PlayerHand
               key={players[(playerIndex + 4) % players.length].name}
               name={players[(playerIndex + 4) % players.length].name || ""}
             />
           </Box>
-          <Box sx={{ display: "flex", gap: 4, justifyContent: "center" }}>
+          <Box sx={{ display: "flex", gap: 14, justifyContent: "center" }}>
             <PlayerHand
               key={players[(playerIndex + 1) % players.length].name}
               name={players[(playerIndex + 1) % players.length].name || ""}
             />
+
+            <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 4 }}>
+              <Box sx={{ pt: "2em", pb: "2em" }}>
+                <Box>Discard</Box>
+                <Box sx={{ "&:hover": { cursor: "pointer" }, height: "5em" }} component={"img"} src={backCard} />
+              </Box>
+            </Box>
+
             <PlayerHand
               key={players[(playerIndex + 2) % players.length].name}
               name={players[(playerIndex + 2) % players.length].name || ""}
